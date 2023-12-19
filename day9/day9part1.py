34a604e66bc8501649540d84cac73e00c6548272
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2023-12-19 23:25:15 +0100

    print reversed forecasted list.

--- day9/day9part1.py
+++ day9/day9part1.py
@@ -17,30 +17,28 @@ def extrapolate_history(history):
     # print(two_dimensional_difference_list)
     # add initial zero
     two_dimensional_difference_list[-1].append(0)
-    print(two_dimensional_difference_list)
+    reversed_difference_list = two_dimensional_difference_list[::-1]
+    # print(reversed_difference_list)
 
-    for row_index, row in enumerate(reversed(two_dimensional_difference_list)):
-        if row_index == len(two_dimensional_difference_list)-1:
+    for row_index, row in enumerate(reversed_difference_list):
+        if row_index == len(reversed_difference_list)-1:
             continue  # Skip the last row since there is no next row
 
-        print(row)
         # a + difference = c
         # d
         # ac
         # dfg
         # ....
-        difference = reversed(two_dimensional_difference_list)[row_index][-1]
-        a = reversed(two_dimensional_difference_list)[row_index + 1][-1]
+        difference = reversed_difference_list[row_index][-1]
+        a = reversed_difference_list[row_index + 1][-1]
         c = a + difference
-        reversed(two_dimensional_difference_list)[row_index + 1].append(c)
-    print('Updated list')
-    for list in two_dimensional_difference_list:
+        reversed_difference_list[row_index + 1].append(c)
+    forecasted_difference_list = reversed_difference_list[::-1]
+    print('Updated reversed list:')
+    for list in forecasted_difference_list:
         print(list)
 
 
-         
-
-        
     # save final next value (top row, predicted number)
 
 
@@ -53,14 +51,15 @@ def recursive_difference_calculator(list_of_numbers, two_dimensional_difference_
             two_dimensional_difference_list.append(difference_list)
             if(sum(difference_list) == 0):
                 for list in two_dimensional_difference_list:
-                     print(list)
+                    print(list)
                 return 
             else:
                 # print(two_dimensional_difference_list)
                 recursive_difference_calculator(difference_list, two_dimensional_difference_list)
             
-example_history = [24, 38, 52, 66, 80, 94, 108, 122, 136, 150, 164, 178, 192, 206, 220, 234, 248, 262, 276, 290, 304]
-example_history = [5, 4, 13, 36, 73, 128, 229, 462, 1032, 2399, 5601, 12970, 29584, 66034, 143565, 303669, 626287, 1264754, 2512757, 4930676, 9578227]
+example_history1 = [24, 38, 52, 66, 80, 94, 108, 122, 136, 150, 164, 178, 192, 206, 220, 234, 248, 262, 276, 290, 304]
+example_history2 = [5, 4, 13, 36, 73, 128, 229, 462, 1032, 2399, 5601, 12970, 29584, 66034, 143565, 303669, 626287, 1264754, 2512757, 4930676, 9578227]
+example_history3 = [10, 13, 16, 21, 30, 45]
 
 list_of_histories = extract_from_input()
-extrapolate_history(example_history)
+extrapolate_history(example_history3)

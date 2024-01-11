607f5969067e43c2035406650cd69e149e55d30a
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2024-01-11 18:34:50 +0100

    look up direction.

--- day16/day16part1.py
+++ day16/day16part1.py
@@ -12,12 +12,15 @@ directions = {
 def mirror_stepper(current_pos: list, direction: list):
     # check current character
     current_character = layout[current_pos[0]][current_pos[1]]
+    direction_symbol = next((key for key, value in directions.items() if value == direction), None)
 
     # next step depending on the character
     if(current_character == '.'):
         # keep going in same direction
-        layout[current_pos[0]][current_pos[1]] = '#'
-        mirror_stepper(current_pos[0] + direction[0], current_pos[1] + direction[1] )
+        layout[current_pos[0]][current_pos[1]] = direction_symbol
+        mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction )
+    elif(current_character == '#'):
+        print('test')
     else:
         print('END')
 
@@ -34,3 +37,5 @@ with open('/Users/friedrichtenhagen/coding/advent_of_code_2023/day16/debug.txt')
     
     for char_list in layout:
         print(char_list)
+    # start in left upper corner, going right
+    mirror_stepper([0, 0], [0, 1])

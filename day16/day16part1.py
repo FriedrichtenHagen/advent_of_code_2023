32d9a681aac09269dcd8695fbdb2c2b91bdd2d66
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2024-01-14 12:48:36 +0100

    Handle layout borders.

--- day16/day16part1.py
+++ day16/day16part1.py
@@ -8,15 +8,15 @@ directions = {
     '<': [0, -1]
 }
 
-    
-
 def mirror_stepper(current_pos: list, direction: list):
     # check current character
     current_character = layout[current_pos[0]][current_pos[1]]
     direction_symbol = next((key for key, value in directions.items() if value == direction), None)
-
+    print([current_character, direction_symbol])
     # mark current pos on energized table
     energized_layout[current_pos[0]][current_pos[1]] = '#'
+    for line in energized_layout:
+        print(line)
     # is it necessary to mark the number of different directions? 
 
 
@@ -25,9 +25,14 @@ def mirror_stepper(current_pos: list, direction: list):
         # set direction symbol
         layout[current_pos[0]][current_pos[1]] = direction_symbol
 
-        mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction )
-    elif(current_character == '#'):
-        mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction )
+        # boundary check
+        y = current_pos[0] + direction[0]
+        x = current_pos[1] + direction[1]
+        if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+            mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction ) 
+        else:
+            return
+            
     elif(current_character == '\\'):
         
         # change of directions:
@@ -37,7 +42,14 @@ def mirror_stepper(current_pos: list, direction: list):
         # [0, -1] -> [-1, 0]
         # the direction coordinates are switched
         new_direction = [direction[1], direction[0]]
-        mirror_stepper([current_pos[0] + new_direction[0], current_pos[1] + new_direction[1]], new_direction )
+        
+        # boundary check
+        y = current_pos[0] + new_direction[0]
+        x = current_pos[1] + new_direction[1]
+        if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+            mirror_stepper([current_pos[0] + new_direction[0], current_pos[1] + new_direction[1]], new_direction ) 
+        else:
+            return
     elif(current_character == '/'):
         
         # change of directions:
@@ -47,37 +59,77 @@ def mirror_stepper(current_pos: list, direction: list):
         # [0, -1] -> [1, 0]
         # the direction coordinates are switched and multiplied by -1
         new_direction = [direction[1] * -1, direction[0] * -1]
-        mirror_stepper([current_pos[0] + new_direction[0], current_pos[1] + new_direction[1]], new_direction )
+
+        # boundary check
+        y = current_pos[0] + new_direction[0]
+        x = current_pos[1] + new_direction[1]
+        if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+            mirror_stepper([current_pos[0] + new_direction[0], current_pos[1] + new_direction[1]], new_direction ) 
+        else:
+            return
     elif(current_character == '-'):
         if(direction == [1, 0] or direction == [-1, 0]):
-            # beam is split
+            # beam goes left
             new_direction1 = [0, -1]
+            # boundary check
+            y = current_pos[0] + new_direction1[0]
+            x = current_pos[1] + new_direction1[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + new_direction1[0], current_pos[1] + new_direction1[1]], new_direction1 ) 
+            
+            # beam goes right
             new_direction2 = [0, 1]
-            mirror_stepper([current_pos[0] + new_direction1[0], current_pos[1] + new_direction1[1]], new_direction1 )
-            mirror_stepper([current_pos[0] + new_direction2[0], current_pos[1] + new_direction2[1]], new_direction2 )
+            # boundary check
+            y = current_pos[0] + new_direction2[0]
+            x = current_pos[1] + new_direction2[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + new_direction2[0], current_pos[1] + new_direction2[1]], new_direction2) 
+            else:
+                return
 
         elif(direction == [0, 1] or direction == [0, -1]):
             # beam keeps traveling in same direction
-            mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction )
+            # boundary check
+            y = current_pos[0] + direction[0]
+            x = current_pos[1] + direction[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction ) 
+            else:
+                return
         else:
             print('Something went wrong: -')
     elif(current_character == '|'):
         if(direction == [1, 0] or direction == [-1, 0]):
             # beam keeps traveling in same direction
-            mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction )
-
+            # boundary check
+            y = current_pos[0] + direction[0]
+            x = current_pos[1] + direction[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + direction[0], current_pos[1] + direction[1]], direction ) 
+            else:
+                return
         elif(direction == [0, 1] or direction == [0, -1]):
-            # beam is split
-            new_direction1 = [0, -1]
-            new_direction2 = [0, 1]
-            mirror_stepper([current_pos[0] + new_direction1[0], current_pos[1] + new_direction1[1]], new_direction1 )
-            mirror_stepper([current_pos[0] + new_direction2[0], current_pos[1] + new_direction2[1]], new_direction2 )
+            # beam goes up
+            new_direction1 = [-1, 0]
+            # boundary check
+            y = current_pos[0] + new_direction1[0]
+            x = current_pos[1] + new_direction1[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + new_direction1[0], current_pos[1] + new_direction1[1]], new_direction1 ) 
+            
+            # beam goes down
+            new_direction2 = [1, 0]
+            # boundary check
+            y = current_pos[0] + new_direction2[0]
+            x = current_pos[1] + new_direction2[1]
+            if 0 <= y <= len(layout)-1 and 0 <= x <= len(layout[0])-1:
+                mirror_stepper([current_pos[0] + new_direction2[0], current_pos[1] + new_direction2[1]], new_direction2) 
+            else:
+                return
         else:
             print('Something went wrong: |')
     else:
         print('mistake. character not found')
-
-
 
 with open('/Users/friedrichtenhagen/coding/advent_of_code_2023/day16/debug.txt') as input:
     input = input.read().splitlines()
@@ -89,10 +141,7 @@ with open('/Users/friedrichtenhagen/coding/advent_of_code_2023/day16/debug.txt')
     for char_list in layout:
         print(char_list)
     # only saves the visited, 'energized' fields
-    energized_layout = layout
+    energized_layout = layout[:]
     # start in left upper corner, going right
     mirror_stepper([0, 0], [0, 1])
 
-
-
-# handle edges of fields: next step is not legal!

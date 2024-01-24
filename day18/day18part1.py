c253ce5a13677d18da86d4ac56014e17f6cc2dca
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2024-01-24 21:58:16 +0100

    Finish digging trench.

--- day18/day18part1.py
+++ day18/day18part1.py
@@ -37,24 +37,49 @@ class PoolDigger:
 
             # right
             if current_direction == 'R':
-                # add length times # to current row
-                for l in range(length):
-                    dig_plan[current_row].insert(current_column + 1, '#')
-                # add length times . to all other rows
-                all_other_row_indexes = [row_index for row_index, row in enumerate(dig_plan) if row_index != current_row]
-                for index in all_other_row_indexes:
-                    dig_plan[index].insert(current_column, '.')
+                # add range check
+                if 0 <= current_column + length < len(dig_plan[current_row]):
+                # field exist:
+                    for l in range(length):
+                        dig_plan[current_row][current_column + l + 1] = '#'
+                else:
+                    # field does not exist:
+                    # add length times # to current row
+                    for l in range(length):
+                        if 0 <= current_column + l + 1 < len(dig_plan[current_row]):
+                            # the row exists
+                            dig_plan[current_row][current_column + l + 1] = '#'
+                        else:
+                            # insert # add start of row (since we are going left)
+                            dig_plan[current_row].append('#')
+                            # add length times . to all other rows
+                            all_other_row_indexes = [row_index for row_index, row in enumerate(dig_plan) if row_index != current_row]
+                            for index in all_other_row_indexes:
+                                dig_plan[index].append('#')
                 # update current pos
                 current_position = [current_row, current_column + length]
             # left
             elif current_direction == 'L':
-                # add length times # to current row
-                for l in range(length):
-                    dig_plan[current_row].insert(current_column, '#')
-                # add length times . to all other rows
-                all_other_row_indexes = [row_index for row_index, row in enumerate(dig_plan) if row_index != current_row]
-                for index in all_other_row_indexes:
-                    dig_plan[index].insert(current_column, '.')
+                # add range check
+                if 0 <= current_column - length < len(dig_plan[current_row]):
+                # field exist:
+                    for l in range(length):
+                        dig_plan[current_row][current_column - l - 1] = '#'
+                else:
+                    # field does not exist:
+                    # add length times # to current row
+                    for l in range(length):
+                        if 0 <= current_column - l + 1 < len(dig_plan[current_row]):
+                            # the row exists
+                            dig_plan[current_row][current_column - l - 1] = '#'
+                        else:
+                            # insert # add start of row (since we are going left)
+                            dig_plan[current_row].insert(0, '#')
+                            # add length times . to all other rows
+                            all_other_row_indexes = [row_index for row_index, row in enumerate(dig_plan) if row_index != current_row]
+                            for index in all_other_row_indexes:
+                                dig_plan[index].insert(0, '.')
+
                 # update current pos
                 current_position = [current_row, current_column - length]
             # down
@@ -68,7 +93,7 @@ class PoolDigger:
                     # the used rows need to be created
                     # maybe only some rows already exist
                     for l in range(length):
-                        if 0 <= current_row + l < len(dig_plan):
+                        if 0 <= current_row + l + 1 < len(dig_plan):
                             # the row already exists
                             dig_plan[current_row + l][current_column] = '#'
                         else:
@@ -76,7 +101,8 @@ class PoolDigger:
                             # create row that has the width of current row
                             width_of_current_row = len(dig_plan[current_row])
                             new_row = ['.'] * width_of_current_row
-                            dig_plan.append([new_row])
+                            new_row[current_column] = '#'
+                            dig_plan.append(new_row)
 
                 # update current pos
                 current_position = [current_row + length, current_column]            
@@ -86,24 +112,26 @@ class PoolDigger:
                 if 0 <= current_row - length < len(dig_plan):
                     # the used rows already exist
                     for l in range(length):
-                        dig_plan[current_row - l][current_column] = '#'
+                        dig_plan[current_row - l - 1][current_column] = '#'
                 else:
                     # the used rows need to be created
                     # maybe only some rows already exist
                     for l in range(length):
-                        if 0 <= current_row - l < len(dig_plan):
+                        if 0 <= current_row - l - 1 < len(dig_plan):
                             # the row already exists
-                            dig_plan[current_row - l][current_column] = '#'
+                            dig_plan[current_row - l - 1][current_column] = '#'
                         else:
                             # the row needs to be created
                             # create row that has the width of current row
                             width_of_current_row = len(dig_plan[current_row])
                             new_row = ['.'] * width_of_current_row
+                            new_row[current_column] = '#'
                             dig_plan.insert(0, new_row)
                 # update current pos
                 current_position = [current_row - length, current_column]  
             for dir in dig_plan:
                 print(dir)
+            print('>>>>>>>>>>>>>>>>>>>')
     def fill_trench():
         pass
 

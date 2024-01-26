fcd39e079403caf4b638a7bce2fb4f5653ab2347
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2024-01-26 23:03:08 +0100

    Refactor class

--- day18/day18part1.py
+++ day18/day18part1.py
@@ -2,18 +2,16 @@
 
 
 class PoolDigger:
-    name = 'day 18'
-    INPUT_PATH = '/Users/friedrichtenhagen/coding/advent_of_code_2023/day18/debug.txt'
+    """A class for managing pool digging operations."""
+    
+    def __init__(self, input_path):
+        self.name = 'day 18'
+        self.INPUT_PATH = input_path
+        self.dig_plan = [['S']]
 
-    # dig_plan starts with one dug hole '#'
-    dig_plan = [
-        ['S']
-    ]
-
-    @staticmethod
-    def read_input():
+    def read_input(self):
         instructions = []
-        with open(PoolDigger.INPUT_PATH) as i:
+        with open(self.INPUT_PATH) as i:
             lines = i.read().split('\n')
             for line in lines:
                 sub_commands = line.split()
@@ -24,9 +22,8 @@ class PoolDigger:
                 }
                 instructions.append(instruction_object)
             return instructions
-        
 
-    def dig_border_trench(instructions_list):
+    def dig_border_trench(self, instructions_list):
 
         current_position = [0, 0]
         for instruction in instructions_list:
@@ -40,102 +37,102 @@ class PoolDigger:
             # right
             if current_direction == 'R':
                 # add range check
-                if 0 <= current_column + length < len(PoolDigger.dig_plan[current_row]):
+                if 0 <= current_column + length < len(self.dig_plan[current_row]):
                 # field exist:
                     for l in range(length):
-                        PoolDigger.dig_plan[current_row][current_column + l + 1] = '#'
+                        self.dig_plan[current_row][current_column + l + 1] = '#'
                 else:
                     # field does not exist:
                     # add length times # to current row
                     for l in range(length):
-                        if 0 <= current_column + l + 1 < len(PoolDigger.dig_plan[current_row]):
+                        if 0 <= current_column + l + 1 < len(self.dig_plan[current_row]):
                             # the row exists
-                            PoolDigger.dig_plan[current_row][current_column + l + 1] = '#'
+                            self.dig_plan[current_row][current_column + l + 1] = '#'
                         else:
                             # insert # add start of row (since we are going left)
-                            PoolDigger.dig_plan[current_row].append('#')
+                            self.dig_plan[current_row].append('#')
                             # add length times . to all other rows
-                            all_other_row_indexes = [row_index for row_index, row in enumerate(PoolDigger.dig_plan) if row_index != current_row]
+                            all_other_row_indexes = [row_index for row_index, row in enumerate(self.dig_plan) if row_index != current_row]
                             for index in all_other_row_indexes:
-                                PoolDigger.dig_plan[index].append('#')
+                                self.dig_plan[index].append('#')
                 # update current pos
                 current_position = [current_row, current_column + length]
             # left
             elif current_direction == 'L':
                 # add range check
-                if 0 <= current_column - length < len(PoolDigger.dig_plan[current_row]):
+                if 0 <= current_column - length < len(self.dig_plan[current_row]):
                 # field exist:
                     for l in range(length):
-                        PoolDigger.dig_plan[current_row][current_column - l - 1] = '#'
+                        self.dig_plan[current_row][current_column - l - 1] = '#'
                 else:
                     # field does not exist:
                     # add length times # to current row
                     for l in range(length):
-                        if 0 <= current_column - l + 1 < len(PoolDigger.dig_plan[current_row]):
+                        if 0 <= current_column - l + 1 < len(self.dig_plan[current_row]):
                             # the row exists
-                            PoolDigger.dig_plan[current_row][current_column - l - 1] = '#'
+                            self.dig_plan[current_row][current_column - l - 1] = '#'
                         else:
                             # insert # add start of row (since we are going left)
-                            PoolDigger.dig_plan[current_row].insert(0, '#')
+                            self.dig_plan[current_row].insert(0, '#')
                             # add length times . to all other rows
-                            all_other_row_indexes = [row_index for row_index, row in enumerate(PoolDigger.dig_plan) if row_index != current_row]
+                            all_other_row_indexes = [row_index for row_index, row in enumerate(self.dig_plan) if row_index != current_row]
                             for index in all_other_row_indexes:
-                                PoolDigger.dig_plan[index].insert(0, '.')
+                                self.dig_plan[index].insert(0, '.')
 
                 # update current pos
                 current_position = [current_row, current_column - length]
             # down
             elif current_direction == 'D':
                 # check if new row needs to be added
-                if 0 <= current_row + length < len(PoolDigger.dig_plan):
+                if 0 <= current_row + length < len(self.dig_plan):
                     # the used rows already exist
                     for l in range(length):
-                        PoolDigger.dig_plan[current_row + l][current_column] = '#'
+                        self.dig_plan[current_row + l][current_column] = '#'
                 else:
                     # the used rows need to be created
                     # maybe only some rows already exist
                     for l in range(length):
-                        if 0 <= current_row + l + 1 < len(PoolDigger.dig_plan):
+                        if 0 <= current_row + l + 1 < len(self.dig_plan):
                             # the row already exists
-                            PoolDigger.dig_plan[current_row + l][current_column] = '#'
+                            self.dig_plan[current_row + l][current_column] = '#'
                         else:
                             # the row needs to be created
                             # create row that has the width of current row
-                            width_of_current_row = len(PoolDigger.dig_plan[current_row])
+                            width_of_current_row = len(self.dig_plan[current_row])
                             new_row = ['.'] * width_of_current_row
                             new_row[current_column] = '#'
-                            PoolDigger.dig_plan.append(new_row)
+                            self.dig_plan.append(new_row)
 
                 # update current pos
                 current_position = [current_row + length, current_column]            
             # up
             elif current_direction == 'U':
                 # check if new row needs to be added
-                if 0 <= current_row - length < len(PoolDigger.dig_plan):
+                if 0 <= current_row - length < len(self.dig_plan):
                     # the used rows already exist
                     for l in range(length):
-                        PoolDigger.dig_plan[current_row - l - 1][current_column] = '#'
+                        self.dig_plan[current_row - l - 1][current_column] = '#'
                 else:
                     # the used rows need to be created
                     # maybe only some rows already exist
                     for l in range(length):
-                        if 0 <= current_row - l - 1 < len(PoolDigger.dig_plan):
+                        if 0 <= current_row - l - 1 < len(self.dig_plan):
                             # the row already exists
-                            PoolDigger.dig_plan[current_row - l - 1][current_column] = '#'
+                            self.dig_plan[current_row - l - 1][current_column] = '#'
                         else:
                             # the row needs to be created
                             # create row that has the width of current row
-                            width_of_current_row = len(PoolDigger.dig_plan[current_row])
+                            width_of_current_row = len(self.dig_plan[current_row])
                             new_row = ['.'] * width_of_current_row
                             new_row[current_column] = '#'
-                            PoolDigger.dig_plan.insert(0, new_row)
+                            self.dig_plan.insert(0, new_row)
                 # update current pos
                 current_position = [current_row - length, current_column]  
 
-    def fill_trench():
-        for row_index, row in enumerate(PoolDigger.dig_plan):
-            for column_index, column in enumerate(PoolDigger.dig_plan[row_index]):
-                if PoolDigger.dig_plan[row_index][column_index] == '.':
+    def fill_trench(self):
+        for row_index, row in enumerate(self.dig_plan):
+            for column_index, column in enumerate(self.dig_plan[row_index]):
+                if self.dig_plan[row_index][column_index] == '.':
                     inside_pool_right = False
                     inside_pool_left = False
                     inside_pool_up = False
@@ -143,12 +140,10 @@ class PoolDigger:
                     # check in four directions
                     # if we cross a '#' in all four directions we know that we are inside the pool
 
-                    # add border checking to each direction
-
                     # right
                     fields_to_the_right = len(row) - (column_index + 1)
                     for f in range(fields_to_the_right):
-                        if PoolDigger.dig_plan[row_index][column_index + f + 1] == '#':
+                        if self.dig_plan[row_index][column_index + f + 1] == '#':
                             inside_pool_right = True
                             break
                         else: 
@@ -156,7 +151,7 @@ class PoolDigger:
                     # left
                     fields_to_the_left = column_index
                     for f in range(fields_to_the_left):
-                        if PoolDigger.dig_plan[row_index][column_index - f - 1] == '#':
+                        if self.dig_plan[row_index][column_index - f - 1] == '#':
                             inside_pool_left = True
                             break
                         else: 
@@ -164,39 +159,53 @@ class PoolDigger:
                     # up
                     fields_to_the_top = row_index
                     for f in range(fields_to_the_top):
-                        if PoolDigger.dig_plan[row_index - f - 1][column_index] == '#':
+                        if self.dig_plan[row_index - f - 1][column_index] == '#':
                             inside_pool_up = True
                             break
                         else: 
                             continue
                     # down
-                    fields_to_the_bottom = len(PoolDigger.dig_plan) - (row_index + 1)
+                    fields_to_the_bottom = len(self.dig_plan) - (row_index + 1)
                     for f in range(fields_to_the_bottom):
-                        if PoolDigger.dig_plan[row_index + f + 1][column_index] == '#':
+                        if self.dig_plan[row_index + f + 1][column_index] == '#':
                             inside_pool_down = True
                             break
                         else: 
                             continue
 
                     if inside_pool_right and inside_pool_left and inside_pool_down and inside_pool_up:
-                        PoolDigger.dig_plan[row_index][column_index] = '#'
+                        self.dig_plan[row_index][column_index] = '#'
 
-
-    def print_trench_plan():
-        for dir in PoolDigger.dig_plan:
+    def print_trench_plan(self):
+        for dir in self.dig_plan:
             print(dir)
         print('>>>>>>>>>>>>>>>>>>>')
+
+    def count_trench_volume(self):
+        ''' counts the # fields in the filed trench '''
+        trench_square_meters = 0
+        for row in self.dig_plan:
+            for character in row:
+                if character == '#':
+                    trench_square_meters += 1
+        return trench_square_meters
 
 
 
 
 if __name__ == "__main__":
-    instructions_list = PoolDigger.read_input()
-    for dir in instructions_list:
-        print(dir)
-
-    PoolDigger.dig_border_trench(instructions_list)
-    PoolDigger.print_trench_plan()
+    input_path = '/Users/friedrichtenhagen/coding/advent_of_code_2023/day18/input.txt'
+    # instance of PoolDigger class with the specified input_path
+    pool_digger = PoolDigger(input_path)
     
-    PoolDigger.fill_trench()
-    PoolDigger.print_trench_plan()
+    instructions_list = pool_digger.read_input()
+    pool_digger.dig_border_trench(instructions_list)
+    print('trench border')
+    pool_digger.print_trench_plan()
+    
+    pool_digger.fill_trench()
+    print('filled trench:')
+    pool_digger.print_trench_plan()
+
+    trench_volume = pool_digger.count_trench_volume()
+    print(f'The trench has a volume of: {trench_volume}')
